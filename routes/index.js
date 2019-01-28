66fd6b68d3c6ece25a2e0a5fba97fb35911cb9ec
james.rodgers <jcm.rodger@gmail.com>
2019-01-28 11:45:30 +0000

    Make test endpoint

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
+var url = require('url');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.all('/*', function (req, res, next) {
+  console.log(req.body)
+  var payload = {
+    "url": url.parse(req.url),
+    "method": req.method,
+    "body": req.body
+  };
+
+  res.send(payload);
 });
 
 module.exports = router;
